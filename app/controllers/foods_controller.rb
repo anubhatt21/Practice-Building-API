44d34371ca1e245cdb5bef6eeae7c1548a7136aa
Anuradha Bhatt <anudave21@gmail.com>
2017-04-04 17:59:55 -0500

    Building RESTful API with create action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,4 +8,13 @@ class FoodsController < ApplicationController
     @food = Food.find_by(id: params[:id])
     render "show.json.jbuilder"
   end
+
+  def create 
+    @food = Food.new(
+      name: params[:name],
+      origin: params[:origin]
+      )
+    @food.save
+    redirect_to "/foods/#{@food.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/foods" => "foods#index"
   get "/foods/:id" => "foods#show"
+  post "/foods" => "foods#create"
 
 end
